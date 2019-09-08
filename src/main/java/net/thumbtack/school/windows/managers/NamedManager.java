de1485077e1a1f95b97053a8ccf412f7876f311d
Storozhev Denis <storozhevdj@gmail.com>
2019-09-08 22:22:50 +0600

    Lesson 7 Release for check

---
+++ src/main/java/net/thumbtack/school/windows/managers/NamedManager.java
@@ -0,0 +1,22 @@
+package net.thumbtack.school.windows.managers;
+
+import net.thumbtack.school.windows.v4.base.Window;
+import net.thumbtack.school.windows.v4.base.WindowException;
+
+public class NamedManager<T extends Window> extends Manager {
+
+    private String name;
+
+    public NamedManager(Window win, String name) throws WindowException {
+        super(win);
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
