f9f0f4a9569781ab33a125ead4392987395b3b3a
Storozhev Denis <storozhevdj@gmail.com>
2019-09-09 20:53:34 +0600

    Lesson 8 Release for check

---
+++ src/main/java/net/thumbtack/school/ttschool/TrainingErrorCode.java
@@ -0,0 +1,7 @@
+package net.thumbtack.school.ttschool;
+
+public enum TrainingErrorCode {
+
+    TRAINEE_WRONG_FIRSTNAME, TRAINEE_WRONG_LASTNAME, TRAINEE_WRONG_RATING
+
+}
---
+++ src/main/java/net/thumbtack/school/ttschool/TrainingException.java
@@ -0,0 +1,17 @@
+package net.thumbtack.school.ttschool;
+
+public class TrainingException extends Exception {
+
+    private static final long serialVersionUID = -3903342712408134702L;
+
+    private TrainingErrorCode trainingErrorCode;
+
+    public TrainingException(TrainingErrorCode trainingErrorCode) {
+        this.trainingErrorCode = trainingErrorCode;
+    }
+
+    public TrainingErrorCode getErrorCode() {
+        return trainingErrorCode;
+    }
+
+}
